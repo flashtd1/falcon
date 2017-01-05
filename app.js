b4cebb62dae7c7fbd8bfb2175bcf8ac8fe2e7428
flashtd1 <flashtd1@163.com>
2017-01-05 16:39:36 +0800

    添加了兼容es7的babel解决方案

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 'use strict'
 
 require('babel-core/register')({
-	presets: ['es2015-node5', 'stage-3']
+	presets: ['es2015-node5', 'stage-3'],
+	plugins: ['transform-runtime']
 })
 
 require('./server')
